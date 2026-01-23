116ed53d8cfbf33d85a218f386b11f3d9e6ea7b2
lksprado <lks-prado@live.com>
2026-01-22 21:45:15 -0300

    update no loader do airflow

--- include/utils/db_interactors.py
+++ include/utils/db_interactors.py
@@ -1,13 +1,15 @@
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from include.utils.logger_cfg import logger
-from sqlalchemy import create_engine 
-import pandas as pd
-from pathlib import Path
-import os
 import shutil
+from pathlib import Path
+from typing import Literal
+
+import pandas as pd
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from sqlalchemy import create_engine
+
+from include.utils.logger_cfg import logger
 
 
-def execute_query(query:str):
+def execute_query(query: str):
     postgres_hook = PostgresHook(postgres_conn_id="postgres_dw")
     conn = postgres_hook.get_conn()
     cursor = conn.cursor()
@@ -15,7 +17,8 @@ def execute_query(query:str):
     conn.commit()
     conn.close()
 
-def send_csv_df_to_db(file_path:Path, table_name,schema, how='replace'):
+
+def send_csv_df_to_db(file_path: Path, table_name, schema, how="replace"):
     """Envia um Dataframe Pandas para camada raw interpretando como texto.
 
     Args:
@@ -26,13 +29,72 @@ def send_csv_df_to_db(file_path:Path, table_name,schema, how='replace'):
     """
     logger.info(f"Iniciando carga na tabela {schema}.{table_name}")
     postgres_hook = PostgresHook(postgres_conn_id="postgres_dw")
-    engine = create_engine('postgresql+psycopg2://', creator=postgres_hook.get_conn)
+    engine = create_engine("postgresql+psycopg2://", creator=postgres_hook.get_conn)
     df = pd.read_csv(file_path, dtype=str)
     try:
         df.to_sql(table_name, engine, if_exists=how, index=False, schema=schema)
-    except:
-        raise logger.error(f"Falha na carga de Dataframe em: {schema}.{table_name}")
-    
+    except Exception as e:
+        raise Exception(f"Failed to load data to database: {e}")
+
+
+def send_single_batch_df_to_db(
+    dir: Path,
+    file_extension: Literal["json", "csv"],
+    schema: str,
+    table_name: str,
+    how="replace",
+):
+    """Envia um Dataframe Pandas para camada raw interpretando como texto.
+
+    Args:
+        dataframe (pd.DataFrame): Dados
+        table_name (_type_): Tabela no BD
+        schema (_type_): Schema no DB
+        how (str, optional): _description_. Defaults to 'replace'.
+    """
+    logger.info(f"Iniciando carga na tabela {schema}.{table_name}")
+    postgres_hook = PostgresHook(postgres_conn_id="postgres_dw")
+    engine = create_engine("postgresql+psycopg2://", creator=postgres_hook.get_conn)
+    # Lê todos os CSVs e concatena
+    input_dir = Path(dir)
+
+    dfs = []
+    for file in input_dir.iterdir():
+        if not file.is_file():
+            continue
+
+        if file.suffix.lower() != f".{file_extension}":
+            continue
+
+        if file_extension == "csv":
+            df = pd.read_csv(file, encoding="utf-8")
+
+        elif file_extension == "json":
+            df = pd.read_json(file, encoding="utf-8")
+
+        else:
+            continue
+
+        df["source_filename"] = file.name
+        dfs.append(df)
+    if not dfs:
+        logger.warning("No files found.")
+        return
+
+    df_final = pd.concat(dfs, ignore_index=True)
+
+    # Insere tudo de uma vez
+    try:
+        df_final.to_sql(
+            name=table_name, con=engine, schema=schema, if_exists=how, index=False
+        )
+        row_count = len(df_final)
+        logger.info(f"Load complete! {row_count} registers.")
+    except Exception as e:
+        raise Exception(f"Failed to load data to database: {e}")
+
+    engine.dispose()
+
 
 def move_files_after_loading(staging_dir: Path, bronze_dir: Path):
     staging_dir = Path(staging_dir)
@@ -51,7 +113,9 @@ def move_files_after_loading(staging_dir: Path, bronze_dir: Path):
         logger.info(f"Movido {file.name} para Bronze")
 
     remaining = len(list(staging_dir.glob("*.json")))
-    logger.info(f"{len(json_files)} arquivos movidos. {remaining} arquivos restantes em {staging_dir}")
+    logger.info(
+        f"{len(json_files)} arquivos movidos. {remaining} arquivos restantes em {staging_dir}"
+    )
 
     csv_files = list(staging_dir.glob("*.csv"))
     if not csv_files:
@@ -62,6 +126,7 @@ def move_files_after_loading(staging_dir: Path, bronze_dir: Path):
         f.unlink()  # remove o arquivo
         logger.info(f"Arquivo removido: {f.name}")
 
+
 def export_table_to_csv(output_dir: Path, filename, query):
     output_dir = Path(output_dir)
     postgres_hook = PostgresHook(postgres_conn_id="postgres_dw")
@@ -70,9 +135,9 @@ def export_table_to_csv(output_dir: Path, filename, query):
     cursor.execute(query)
     rows = cursor.fetchall()
     colnames = [desc[0] for desc in cursor.description]
-    df_export = pd.DataFrame(rows, columns=colnames) 
+    df_export = pd.DataFrame(rows, columns=colnames)
     file_dest = output_dir / filename
-    df_export.to_csv(file_dest, index=False)       
+    df_export.to_csv(file_dest, index=False)
     conn.commit()
     cursor.close()
     conn.close()
